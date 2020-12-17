ac23f12313e9c3eea61bf9c6bb31f924fff75e06
Omar bouzinab <omar@Omars-MacBook-Air.local>
2020-12-17 18:06:10 +0100

    finished

--- main.c
+++ main.c
@@ -8,10 +8,32 @@
 
 int main(int argc, char * argv[]) {
 
+    if(argc == 2) {
+        if(strcmp(argv[1], "help") == 0) {
+            puts("Compression: ./huff encode fichiersource.txt [fichierdest.bin]");
+            puts("Decompression: ./huff decode fichiersource.bin [fichierdest.text]");
+            puts("[]: optionel");
+        }else{
+            printf("Erreur: option %s inexistante.", argv[1]);
+
+        }
+        exit(EXIT_FAILURE);
+
+    }
+
+
     if(argc < 4){
         puts("Erreur: nombre d'arguments insuffisant. Veuillez reessayer.");
         exit(EXIT_FAILURE);
     }
+
+    char * src = argv[2];
+    char * dest;
+    if(argc == 3) 
+        dest = src;
+    else 
+        dest = argv[3];
+
     
     if(strcmp(argv[1], "encode")==0) {
         puts("Compression en cours...");
